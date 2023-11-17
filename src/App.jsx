09a77342a0f7090a4448814c6e4056f3931b1fbe
Mohsin Raza <zmohsinraza64@gmail.com>
2023-11-16 23:27:56 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+
+function App() {
+
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl:'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  let counter = 5
+  const addvalue = () =>{
+    console.log("clicked", counter);
+    counter = counter + 1
+  }
+  return (
+    <>
+    <h3>{user.name}</h3>
+    <img
+    className="avatar"
+    src={user.imageUrl}
+    alt={'Photo of' + user.name}
+    style={{
+      width: user.imageSize,
+      height: user.imageSize
+    }}
+    />
+    <h1>Icerativez</h1>
+    <h2>About</h2>
+      <p>Hello there.<br />How do you do?</p>
+      <button>Next </button>
+      <br />
+
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addvalue}>add value </button>
+      <button>remove value</button>
+      <p>
+        git
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nostrum doloremque laborum labore minus? Qui fugiat aliquid quisquam odio ipsum itaque veritatis alias repellendus similique. Veritatis nesciunt consequatur aspernatur. Debitis!
+      </p>
+    </>
+  )
+}
+
+export default App
